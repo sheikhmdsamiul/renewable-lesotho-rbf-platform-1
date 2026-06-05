139158c7f7fec2ea62aa83f154552a32ee143bc0
Sheikh Md. Samiul <sk.mohammad.samiul@gmail.com>
2026-06-05 17:53:08 +0600

    Fix media file serving: bypass static() DEBUG check with raw re_path + serve view

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from .views import HealthCheckView
 from rbf.projects.views import (
@@ -31,4 +32,6 @@ urlpatterns = [
     path('api/notifications/', include('rbf.notifications.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
